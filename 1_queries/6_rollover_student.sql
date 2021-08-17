fe758141290810be6f30cfda83dca38fae04a925
aphy18 <aphason1@.com>
2021-08-17 04:47:24 +0000

    added two queries

---
+++ 1_queries/6_rollover_student.sql
@@ -0,0 +1,6 @@
+SELECT students.name,students.start_date as student_start_date, cohorts.name, cohorts.start_date as cohort_start_date
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id 
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
+
